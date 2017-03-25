02b487f52e029db8ea655171bb44838d662e8f55
Brianne Barta <brianne28@ufl.edu>
2017-03-24 20:47:20 -0400

    send form inputs to console

--- js/main.js
+++ js/main.js
@@ -10,3 +10,22 @@ function myFunction() {
     count++;
     p.innerHTML = count;
 };
+
+
+
+var nameInput1 = document.getElementById('firstname');
+var nameInput2 = document.getElementById('lastname');
+var nameInput3 = document.getElementById('email');
+var nameInput4 = document.getElementById('message');
+
+
+document.querySelector('form.event').addEventListener("submit", function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    console.log(nameInput1.value);
+    console.log(nameInput2.value);
+    console.log(nameInput3.value);
+    console.log(nameInput4.value);
+});
