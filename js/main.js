0c5bab70b40bf7749f65fe45c0e37c4c7c8a740e
Brianne Barta <brianne28@ufl.edu>
2017-03-24 21:24:24 -0400

    add trivia questions and event listeners

--- js/main.js
+++ js/main.js
@@ -29,3 +29,17 @@ document.querySelector('form.event').addEventListener("submit", function (e) {
     console.log('The email is ' + nameInput3.value);
     console.log('The message is ' + nameInput4.value);
 });
+
+
+
+document.getElementById("q1").addEventListener("click", function(){
+    document.getElementById("a1").innerHTML = "1853";
+});
+
+document.getElementById("q2").addEventListener("click", function(){
+    document.getElementById("a2").innerHTML = "W. Kent Fuchs";
+});
+
+document.getElementById("q3").addEventListener("click", function(){
+    document.getElementById("a3").innerHTML = "52,519";
+});
